5c3d84b5ad83c53d21c9efd6a05a81f23b9f5c08
US\joseph.henning <joseph.henning@schwab.com>
2018-06-05 15:02:12 -0700

    Deploy.sh update

--- deploy.cmd
+++
@@ -1,2 +0,0 @@
-kubectl apply -f kubernetes/mysql.yml
-kubectl apply -f kubernetes/web.yml
---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -xe
+
+echo "Deploying pet-db..."
+kubectl apply -f kubernetes/mysql.yml --namespace default
+
+IMAGE_VERSION=${GO_PIPELINE_LABEL:-latest}
+PROJECT_ID=${GCLOUD_PROJECT_ID:-spatial-groove-205401}
+echo "Deploying pet-web image version: $WEB_IMAGE_VERSION"
+
+cat kubernetes/web.yml | sed "s/\(image: \).*$/\1us.gcr.io\/$PROJECT_ID\/pet-app:$IMAGE_VERSION/" | kubectl apply -f - --namespace default
